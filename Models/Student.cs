46d2362607c5798d37da843b6cf5e02b08e5f5ad
CristianRad <cristian.mihai.rad@gmail.com>
2020-05-10 11:50:07 +0300

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1.Models
+{
+    public class Student
+    {
+        public long Id { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public Boolean Repeater { get; set; }
+        [Range(1.0, 10.0)]
+        public double Average { get; set; }
+    }
+}
